625c90916b290c9b29def7370ab761f6f9a1b9ed
gzh <936285702@qq.com>
2021-08-06 17:23:03 +0800

    完成歌手列表滚动条

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -2,14 +2,37 @@
  * @Author: GZH
  * @Date: 2021-08-06 10:46:15
  * @LastEditors: GZH
- * @LastEditTime: 2021-08-06 10:47:43
+ * @LastEditTime: 2021-08-06 17:08:16
  * @FilePath: \cloud-music\src\application\Singers\index.js
  * @Description:
  */
-import React from 'react';
-
+import React, { useState } from 'react';
+import Horizen from '../../baseUI/horizen-item';
+import { categoryTypes, alphaTypes } from '../../api/config';
+import { NavContainer } from './style';
 function Singers(props) {
-  return <div>Singers</div>;
+  let [category, setCategory] = useState('');
+  let [alpha, setAlpha] = useState('');
+
+  let handleUpdateAlpha = val => {
+    setAlpha(val);
+  };
+
+  let handleUpdateCatetory = val => {
+    setCategory(val);
+  };
+
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={'分类 (默认热门):'}
+        handleClick={handleUpdateCatetory}
+        oldVal={category}
+      ></Horizen>
+      <Horizen list={alphaTypes} title={'首字母:'} handleClick={val => handleUpdateAlpha(val)} oldVal={alpha}></Horizen>
+    </NavContainer>
+  );
 }
 
 export default React.memo(Singers);
---
+++ src/application/Singers/style.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: GZH
+ * @Date: 2021-08-06 17:03:56
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-08-06 17:03:57
+ * @FilePath: \cloud-music\src\application\Singers\style.js
+ * @Description:
+ */
+import styled from 'styled-components';
+import style from '../../assets/global-style';
+
+export const NavContainer = styled.div`
+  box-sizing: border-box;
+  position: fixed;
+  top: 95px;
+  width: 100%;
+  padding: 5px;
+  overflow: hidden;
+`;
